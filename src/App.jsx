11a426e0234b35e41645ed49f88e4c360947e2ef
Arnab Paul <arnab2181@gmail.com>
2025-08-22 23:44:43 +0530

    Report Page and Dashboard Page is prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WelcomeMessage from "./Authentication/Welcome/WelcomeMessage";
-import LoginPage from "./Authentication/Login/LoginPage";
-import RegisterPage from "./Authentication/Register/RegisterPage";
-import InvoiceViewer from "./Layout/Display/InvoiceViewer";
+
 import ReportViewer from "./Layout/Display/ReportViewer";
-import CustomerViewer from "./Layout/Display/CustomerViewer";
+
 import DashboardViewer from "./Layout/Display/DashboardViewer";
-import PaymentViewer from "./Layout/Display/PaymentViewer";
-import "./App.css";
+
+
 
 function App() {
   return (
@@ -15,12 +12,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<DashboardViewer/>}/>   // Home route with Navbar - will change based on the FireBase integration , mainly Authentication and Cloud Firestore
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path = "/invoices" element={<InvoiceViewer />} />
+          
           <Route path = "/reports" element={<ReportViewer />} />
-          <Route path = "/customers" element={<CustomerViewer />} />
-          <Route path = "/payments" element={<PaymentViewer />} />
+          
           
         </Routes>
       </Router>
