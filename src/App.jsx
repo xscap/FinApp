9dc5b3f114b3a101375aaae3d3eabffde5711065
Arnab Paul <arnab2181@gmail.com>
2025-08-23 00:19:35 +0530

    Invoice Page is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-
-
-import './App.css'
+import DashboardViewer from "./Layout/Display/DashboardViewer";
+import ReportViewer from "./Layout/Display/ReportViewer";
+import InvoiceViewer from "./Layout/Display/InvoiceViewer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<DashboardViewer/>}/>   // Home route with Navbar - will change based on the FireBase integration , mainly Authentication and Cloud Firestore
           
           <Route path = "/reports" element={<ReportViewer />} />
+          <Route path = "/invoices" element={<InvoiceViewer />} />
           
           
         </Routes>
